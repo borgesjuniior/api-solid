32e2ef8b049d3e8dfd1297a00456f1cc9cdca36b
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-10-03 21:25:06 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 export const app = Fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Júnior',
+    email: 'valdecyborgesjr@example.com',
+  },
+});
