583b201c9f610c988d2f813f6bf4025cdb8edf7f
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-10-08 21:52:31 -0300

    Controllers & Testes E2E - Princípios da autenticação JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register-controller';
+import { profile } from './controllers/profile-controller';
+import { authenticate } from './controllers/authenticate-controller';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
+  app.post('/sessions', authenticate);
+
+  app.get('/me', profile);
 }
