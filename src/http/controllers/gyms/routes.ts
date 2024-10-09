e89544cbac50a4d29af082fea3b67eecbf02e790
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-10-09 20:21:09 -0300

    Controllers & Testes E2E - Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify';
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
+import { search } from './search-gyms-controller';
+import { nearby } from './nearby-controller';
+import { create } from './create-gym-controller';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
+  app.get('/gyms/nearby', nearby);
+
+  app.post('/gyms', create);
 }
