d663bd628d90ba46a73bc2b797afe9169b16c648
Valdecy Borges Da Costa Junior <valdecyborgesjr@hotmail.com>
2024-10-10 17:52:03 -0300

    fix: add checkInRoutes and gymsRoutes to app

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import { appRoutes } from './http/routes';
 import { ZodError } from 'zod';
 import { env } from './env';
 import { fastifyJwt } from '@fastify/jwt';
+import { gymsRoutes } from './http/controllers/gyms/routes';
+import { checkInsRoutes } from './http/controllers/check-ins/routes';
 
 export const app = Fastify();
 
 app.register(appRoutes);
+app.register(checkInsRoutes);
+app.register(gymsRoutes);
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
